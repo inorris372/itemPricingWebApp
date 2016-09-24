b0669e55ad8f8a25674da4848eb471744f03148b
Ian Norris <inorris372@gmail.com>
2016-09-24 00:52:27 -0500

    Continued configuring user password encryption and hashing.

--- src/models/users/views.py
+++ src/models/users/views.py
@@ -1,4 +1,11 @@
 from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+from werkzeug.utils import redirect
+
+from src.models.users.user import User
 
 __author__ = 'Ian'
 
@@ -6,9 +13,17 @@ __author__ = 'Ian'
 user_blueprint = Blueprint('users', __name__)
 
 
-@user_blueprint.route('/login')
+@user_blueprint.route('/login', methods=['GET', "POST"])
 def login_user():
-    pass
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        if User.validate_login(email, password):
+            session['email'] = email
+            return redirect(url_for(".user_alerts"))
+
+    return render_template("users/login.html") # send the user an error if their login was invalid
 
 
 @user_blueprint.route('/register')
